77b9d056e7cd646acb0a1a215153bae2486bf7c9
life4gal <life4gal@gmail.com>
2023-02-28 15:49:49 +0800

    Fix standalone test case.

--- standalone_test/src/main.cpp
+++ standalone_test/src/main.cpp
@@ -1,35 +1,21 @@
-#include <CMakeTemplateProject/easy_network.hpp>
-#include <CMakeTemplateProject/hello.hpp>
-#include <iostream>
+import std;
+import gal.utility;
+
+#include <macro.hpp>
 
 auto main() -> int
 {
 	std::cout << "Hello CMakeTemplateProject!"
-			  << "\nCompiler Name: " << CMakeTemplateProject_COMPILER_ID
-			  << "\nCompiler Version: " << CMakeTemplateProject_COMPILER_VERSION
-			  << "\nCTP Version: " << CMakeTemplateProject_VERSION
-			  << "\nAnswer: " << ctp::answer() << '\n'
-			  << "\nMD5 of string [\"CMakeTemplateProject\"]: " << ctp::md5("CMakeTemplateProject") << '\n';
-
-	ctp::log_me();
-
-	std::cout << std::boolalpha << ctp::EasyNetwork::https_support_check() << '\n';
+			  << "\nCompiler Name: " << G_COMPILER_ID
+			  << "\nCompiler Version: " << G_COMPILER_VERSION
+			  << "\nCTP Version: " << G_VERSION
+			  << "\n";
 
 	{
-		int	  x{-1};
-		int	  y{-1};
-		int	  channels{-1};
-		auto* data = ctp::load_image(TEST_PNG_FILE_PATH, &x, &y, &channels);
-		std::cout
-				<< "test.png: \n"
-				<< "\tdata: " << static_cast<void*>(data) << '\n'
-				<< "\tx: " << x << '\n'
-				<< "\ty: " << y << '\n'
-				<< "\tchannels: " << channels << '\n';
-		ctp::free_image(data);
-	}
+		using format_type_1 = GAL_GUI_TEMPLATE_STRING_TYPE("{} is not {}");
+		GAL_GUI_ASSUME(std::format(format_type_1::value, "hello", "world") == "hello is not world");
 
-	{
-		std::cout << "fontconfig version: " << ctp::fontconfig_version() << '\n';
+		using format_type_2 = GAL_GUI_TEMPLATE_STRING_TYPE("{:<10} == {:>10}");
+		GAL_GUI_ASSUME(std::format(format_type_2::value, "hello", "world") == "hello      ==      world");
 	}
 }
